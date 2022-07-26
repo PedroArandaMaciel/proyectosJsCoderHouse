2f67430eca6fba781981651cd3755735c8fc1a80
Pedro Aranda Maciel <pedroarandamaciel2@gmail.com>
2022-07-26 16:30:27 -0300

    Se agregaron funciones para calcular los precios de cada pelicula y se hicieron nuevos condicionales para validar que la cantidad de mayores y menores sea minimo uno. Mas ediciones en general

--- script.js
+++ script.js
@@ -1,16 +1,28 @@
-let pelicula, cantMayores, cantMenores, pregunta
-let repetir = true
+function suma(numero1 = 0, numero2 = 0) {
+    return numero1 + numero2
+}
+function entradaMayores(cantidadDeEntradas = 1) {
+    const PRECIO = 900
+    return cantidadDeEntradas * PRECIO
+}
+function entradaMenores(cantidadDeEntradas = 1) {
+    const PRECIO = 450
+    return cantidadDeEntradas * PRECIO
+}
 alert("Bienvenido al sistema de reserva de entradas")
+let repetir = true
 while (repetir) {
+    let pelicula, cantMayores, cantMenores, pregunta, precioReservaMayores = 0, precioReservaMenores = 0, precioFinal = 0
+
     pelicula = parseInt(prompt("SISTEMA DE RESERVA DE ENTRADAS\n Por favor escriba el numero segun corresponda\n 1) El Telefono Negro\n 2) La Casa Embrujada 2\n 3)Los Minions\n 4)Avatar, El Camino Del Agua"))
     cantMayores = parseInt(prompt("Cuantas entradas para MAYORES desea reservar?"))
     cantMenores = parseInt(prompt("Cuantas entradas para MENORES desea reservar?"))
 
-    if (isNaN(cantMayores) || isNaN(cantMenores)) {
-        alert("Cantidad ingresada no valida, por favor intente nuevamente utilizando solo numeros")
+    if ((isNaN(cantMayores) || isNaN(cantMenores)) || (cantMayores === 0 && cantMenores === 0)) {
+        alert("Cantidad de entradas ingresada no valida, por favor intente nuevamente")
+        console.log("Cantidad no validad")
         continue
     }
-
     switch (pelicula) {
         case 1:
             pelicula = "El Telefono Negro"
@@ -30,10 +42,21 @@ while (repetir) {
             continue
     }
     alert(`La pelicula seleccionada es ${pelicula}. Para ${cantMayores} mayores y ${cantMenores} menores`)
-    console.log(`La pelicula seleccionada es ${pelicula}. Para ${cantMayores} mayores y ${cantMenores} menores`)
+    
+    if (cantMayores > 0) {
+        precioReservaMayores = entradaMayores(cantMayores)
+    }
+    if (cantMenores > 0) {
+        precioReservaMenores = entradaMenores(cantMenores)
+    }
 
-    pregunta = prompt("Gracias por su reversa! Desea realizar otra? (SI/NO)").toLowerCase()
-    if (pregunta == "no" || pregunta == "no gracias") {
+    precioFinal = suma(precioReservaMayores, precioReservaMenores)
+    console.log(`\n*********PELICULA: ${pelicula}\n*\n* ADULTOS: ${cantMayores} Mayores-------->SubTotal: $${precioReservaMayores}\n* CHICOS: ${cantMenores} Menores--------->SubTotal: $${precioReservaMenores}\n*\n*                        PRECIO FINAL: $${precioFinal}\n*********************************************`)
+    
+    do {
+        pregunta = prompt("Gracias por su reserva! Desea realizar otra? (SI/NO)").toLowerCase()
+    } while (pregunta != "si" && pregunta != "no")
+    if (pregunta == "no") {
         repetir = false
         alert("Gracias por utilizar nuestros servicios, puede revisar su reserva en la consola presionando: F12>console")
     }
