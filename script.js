56cdb4e3f88e7a2f9db012e089e661d43c22ef41
Pedro Aranda Maciel <pedroarandamaciel2@gmail.com>
2022-08-02 22:46:35 -0300

    Se implemento el array para que captara las entradas generadas, se hizo una funcion para borrar reservas del array, y mejoras en general

--- script.js
+++ script.js
@@ -11,9 +11,13 @@ function calcularEntrada(cantidadDeEntradas = 1, edad = "") {
     }
 }
 function entrada(pelicula, cantMayores, precioReservaMayores, cantMenores, precioReservaMenores, precioFinal) {
-    return (`\n*********PELICULA: ${pelicula}\n*\n* ADULTOS: ${cantMayores} Mayores-------->ubTotal: $${precioReservaMayores}\n* CHICOS: ${cantMenores} Menores--------->SubTotal: $${precioReservaMenores}\n*\n*                        PRECIO FINAL: $${precioFinal}\n*********************************************`)
+    return (`\n*********PELICULA: ${pelicula}\n*\n* ADULTOS: ${cantMayores} Mayores-------->SubTotal: $${precioReservaMayores}\n* CHICOS: ${cantMenores} Menores--------->SubTotal: $${precioReservaMenores}\n*\n*                        PRECIO FINAL: $${precioFinal}\n*********************************************`)
 }
-class reserva {
+function cancelarReserva(idReserva) {
+    reservas.splice(idReserva - 1, 1)
+    reservas.push(`La reserva ${idReserva} fue cancelada `)
+}
+class Reserva {
     constructor(pelicula = "", cantMayor = 0, cantMenor = 0, idReserva) {
         this.Pelicula = pelicula;
         this.Mayores = cantMayor;
@@ -22,6 +26,7 @@ class reserva {
     }
 }
 alert("Bienvenido al sistema de reserva de entradas")
+const reservas = []
 let repetir = true, conteoReserva = 1
 while (repetir) {
     let pelicula, cantMayores = 0, cantMenores = 0, pregunta, precioReservaMayores = 0, precioReservaMenores = 0, precioFinal = 0
@@ -54,35 +59,17 @@ while (repetir) {
             continue
     }
     alert(`La pelicula seleccionada es ${pelicula}. Para ${cantMayores} mayores y ${cantMenores} menores`)
-
-    /*const RESERVA = new reserva(pelicula, cantMayores, cantMenores, conteoReserva) //(Codigo a implementar, en revision)
-    const RESERVASarray = []
-    RESERVASarray.push(RESERVA)    //No agrega mas de un objeto, va variando las propiedades en vez de crear uno nuevo (conteoReserva si varia)
+    const reserva = new Reserva(pelicula, cantMayores, cantMenores, conteoReserva)
+    reservas.push(reserva)
     conteoReserva++
-    console.log(RESERVASarray)*/
-
-    function cancelarReserva(idReserva) {
-        RESERVASarray.splice(idReserva - 1, 1)
-    }
-    const RESERVA = new reserva("Avatar, El Camino Del Agua", 2, 3, 1)
-    const RESERVA2 = new reserva("Los Minions", 1, 3, 2)
-    const RESERVA3 = new reserva("La Casa Embrujada", 3, 0, 3)
-    const RESERVA4 = new reserva("El Telefono Negro", 2, 0, 4)
-    const RESERVASarray = [RESERVA, RESERVA2, RESERVA3, RESERVA4]
-    console.log(RESERVASarray[0])
-    cancelarReserva(1)
-    console.log(RESERVASarray)
-
     if (cantMayores > 0) {
         precioReservaMayores = calcularEntrada(cantMayores, "mayor")
     }
     if (cantMenores > 0) {
         precioReservaMenores = calcularEntrada(cantMenores, "menor")
     }
-
     precioFinal = suma(precioReservaMayores, precioReservaMenores)
     console.log(entrada(pelicula, cantMayores, precioReservaMayores, cantMenores, precioReservaMenores, precioFinal))
-
     do {
         pregunta = prompt("Gracias por su reserva! Desea realizar otra? (SI/NO)").toLowerCase()
     } while (pregunta != "si" && pregunta != "no")
@@ -91,3 +78,6 @@ while (repetir) {
         alert("Gracias por utilizar nuestros servicios, puede revisar su reserva en la consola presionando: F12>console")
     }
 }
+console.log(reservas)
+cancelarReserva(1)
+console.log(reservas)
